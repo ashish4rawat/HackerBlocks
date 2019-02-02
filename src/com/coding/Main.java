7826f4e1882d39549871ff3d03880b3e137d094b
ashish <ashish4rawat@gmail.com>
2019-02-02 18:00:25 +0530

    second commit

--- src/com/coding/Main.java
+++ src/com/coding/Main.java
@@ -7,16 +7,14 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner scanner = new Scanner(System.in);
-        String s= scanner.nextLine();
+        int a= scanner.nextInt();
+        int b= scanner.nextInt();
 
-        if(s.charAt(0)>='a' && s.charAt(0)<='z'){
-            System.out.println("lowercase");
-        }
-        else if(s.charAt(0)>='A' && s.charAt(0)<='Z'){
-            System.out.println("UPPERCASE");
-        }else {
-            System.out.println("Invalid");
-        }
+
+
+        int x = (int) (Math.log10(a)/Math.log10(b));
+
+        System.out.println(x);
 
 
     }
