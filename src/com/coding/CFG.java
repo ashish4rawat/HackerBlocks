7bb19306c5b2ce5c3880d1141b4603eac84f19ec
ashish <ashish4rawat@gmail.com>
2019-02-14 17:27:39 +0530

    tree 2

--- src/com/coding/CFG.java
+++ src/com/coding/CFG.java
@@ -2,51 +2,143 @@ package com.coding;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
+
+
+// Java Program to print Bottom View of Binary Tree
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.io.*;
+import java.util.*;
+
+// Tree node class
 class Node
 {
-    int data;
-    Node left, right;
-    Node(int item)
+    int data; //data of the node
+    int hd; //horizontal distance of the node
+    Node left, right; //left and right references
+
+    // Constructor of tree node
+    public Node(int key)
     {
-        data = item;
+        data = key;
+        hd = Integer.MAX_VALUE;
         left = right = null;
     }
 }
+
+
+class BottomView
+{
+    // driver function to test the above functions
+    public static void main(String args[])
+    {
+
+        // Input the number of test cases you want to run
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        //Node root=null;
+        while (t > 0)
+        {
+            HashMap<Integer, Node> m = new HashMap<Integer, Node> ();
+            int n = sc.nextInt();
+            Node root=null;
+            while (n > 0)
+            {
+                int n1 = sc.nextInt();
+                int n2 = sc.nextInt();
+                char lr = sc.next().charAt(0);
+                //  cout << n1 << " " << n2 << " " << (char)lr << endl;
+                Node parent = m.get(n1);
+                if (parent == null)
+                {
+                    parent = new Node(n1);
+                    m.put(n1, parent);
+                    if (root == null)
+                        root = parent;
+                }
+                Node child = new Node(n2);
+                if (lr == 'L')
+                    parent.left = child;
+                else
+                    parent.right = child;
+                m.put(n2, child);
+                n--;
+            }
+
+            GfG g = new GfG();
+
+            //g.leftView(root);
+            g.bottomView(root);
+
+            System.out.println();
+            t--;
+        }
+    }
+}
+
+
+
+/*
+class Node {
+    int data; //data of the node
+    int hd; //horizontal distance of the node
+    Node left, right; //left and right references
+    public Node(int key)
+    {
+        data = key;
+        hd = Integer.MAX_VALUE;
+        left = right = null;
+    }
+}*/
 class GfG
 {
-
-    ArrayList<ArrayList> lists = new ArrayList<>();
-
-    void leftView(Node root)
+    // Should print bottom view of tree with given root
+    public void bottomView(Node root)
     {
         // Your code here
-        ArrayList<ArrayList> lists = new ArrayList<>();
 
-        leftView(root,0,lists);
+        HashMap<Integer,ArrayList<Integer>> map = new HashMap<>();
+        bottomView(root,0,map);
 
-        for (int i = 0; i <lists.size() ; i++) {
-            System.out.print(lists.get(i).get(0)+" ");
+
+        int min = Integer.MAX_VALUE , max = Integer.MIN_VALUE;
+        for (int a:map.keySet()) {
+            if(a<min)
+                min = a;
+            if(a>max)
+                max = a;
+        }
+
+        for (int i = min; i <=max ; i++) {
+            int size = map.get(i).size();
+            System.out.print(map.get(i).get(size-1 ) +" ");
         }
 
     }
 
-    private void leftView(Node node, int level,  ArrayList<ArrayList> lists) {
+    private void bottomView(Node node, int level, HashMap<Integer, ArrayList<Integer>> map) {
 
         if(node==null){
             return;
         }
 
-        if(lists.size()<level+1){
+        if(map.containsKey(level)){
+            map.get(level).add(node.data);
+        }else {
             ArrayList<Integer> list = new ArrayList<>();
             list.add(node.data);
-            lists.add(list);
-        }else {
-            lists.get(level).add(node.data);
+            map.put(level,list);
         }
 
-        leftView(node.left,level+1,lists);
-        leftView(node.right,level+1,lists);
+
+        bottomView(node.left,level-1,map);
+        bottomView(node.right,level+1,map);
 
     }
+
 }
