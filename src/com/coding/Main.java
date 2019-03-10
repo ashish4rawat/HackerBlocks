1ce5e73712de5b927ea54258574cc832ffa9cae4
ashish <ashish4rawat@gmail.com>
2019-03-10 15:14:39 +0530

    new code

--- src/com/coding/Main.java
+++ src/com/coding/Main.java
@@ -7,36 +7,60 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(0,54);
-        list.add(3,54);
+
 
         Scanner scanner = new Scanner(System.in);
         String T = scanner.nextLine();
         int t = Integer.parseInt(T);
         for (int h = 0; h <t ; h++) {
 
-            String N = scanner.next();
-            int n = Integer.parseInt(N);
+            int M= scanner.nextInt();
+            int N= scanner.nextInt();
 
-            String[] strings = new String[n];
+            int[][] A = new int[M][N];
 
-
-            int min = Integer.MAX_VALUE;
-            for (int i = 0; i <n ; i++) {
-                strings[i] = scanner.next();
-                min = Math.min(min,strings[i].length());
+            for (int i = 0; i <M ; i++) {
+                for (int j = 0; j < N; j++) {
+                    A[i][j] = scanner.nextInt();
+                }
             }
 
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            int K = scanner.nextInt();
+
+            int pre = A[x][y];
+            
+            replace(x,y,K,pre,A);
+
+            for (int i = 0; i < M ; i++) {
+                for (int j = 0; j <N ; j++) {
+                    System.out.print(A[i][j]+" ");
+                }
+            }
+
+            System.out.println();
 
         }
 
     }
 
+    private static void replace(int x, int y, int k, int pre, int[][] A) {
+
+        if( (x>=A.length || x<0) || (y>=A[0].length || y<0))
+            return;
+
+        if(A[x][y] != pre)
+            return;
+
+        A[x][y] = k;
+        replace(x,y-1,k,pre,A);
+        replace(x,y+1,k,pre,A);
+        replace(x-1,y,k,pre,A);
+        replace(x+1,y,k,pre,A);
 
 
-
-
+    }
 
 
 }
