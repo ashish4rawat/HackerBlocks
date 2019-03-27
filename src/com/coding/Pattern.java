92814272d48654ddad83e10a868f4740bdb11fa8
ashish <ashish4rawat@gmail.com>
2019-03-27 11:40:56 +0530

    beta bits

--- src/com/coding/Pattern.java
+++ src/com/coding/Pattern.java
@@ -39,6 +39,34 @@ public class Pattern {
 
     }
 
+
+    class Node {
+        int data;
+        Node left, right;
+
+        Node(int item) {
+            data = item;
+            left = right = null;
+        }
+    }
+
+    void mirror(Node node)
+    {
+        // Your code here
+
+        if(node==null)
+            return;
+
+
+        Node temp = node.left;
+        node.left = node.right;
+        node.right  =temp;
+
+        mirror(node.left);
+        mirror(node.right);
+
+    }
+
     private static int jumps(int index, int[] A, int[] mem) {
 
 
