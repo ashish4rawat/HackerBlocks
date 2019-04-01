c803551db669a590c746fb43cdcee6db6beb448f
ashish <ashish4rawat@gmail.com>
2019-04-01 13:13:43 +0530

    as

--- src/com/coding/CFG.java
+++ src/com/coding/CFG.java
@@ -18,111 +18,50 @@ import java.io.*;
 import java.util.*;
 
 
-
-class Node {
-    int data; //data of the node
-    int hd; //horizontal distance of the node
-    Node left, right; //left and right references
-    public Node(int key)
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
     {
         data = key;
-        hd = Integer.MAX_VALUE;
-        left = right = null;
-    }
-}
-
-class Pair{
-    int height,value;
-
-    public Pair(int height, int value) {
-        this.height = height;
-        this.value = value;
+        next = null;
     }
 }
 
 class GfG
 {
-    // Should print bottom view of tree with given root
-    public void bottomView(Node node)
-    {
-        // Your code here
-
-        HashMap<Integer,Pair> map = new HashMap<>();
-
-
-        bottom(node,map,0,0);
-
-        int min = Integer.MAX_VALUE,max = Integer.MIN_VALUE;
-        for (int key:map.keySet()) {
-            min = Math.min(key,min);
-            max = Math.max(key,max);
-        }
-
-        for (int i = min; i <=max ; i++) {
-            System.out.print(map.get( i ).value+" ");
-        }
-
-    }
-
-    private void bottom(Node node, HashMap<Integer, Pair> map,int dist,int height) {
-
-
-        if(node==null)
-            return;
-
-        if(map.containsKey(dist)){
-
-            Pair p = map.get(dist);
-
-            if(p.height<=height){
-                map.put(dist , new Pair(height,node.data));
-            }
-
-        }else {
-            map.put(dist,new Pair(height,node.data));
-        }
-
-
-        bottom(node.left,map,dist-1,height+1);
-        bottom(node.right,map,dist+1,height+1);
-
-    }
-}
-
-/*
-class Node {
-    int data; //data of the node
-    int hd; //horizontal distance of the node
-    Node left, right; //left and right references
-    public Node(int key)
-    {
-        data = key;
-        hd = Integer.MAX_VALUE;
-        left = right = null;
-    }
-}*/
-class GfGf
-{
-
-
-    boolean isIdentical(Node node1, Node node2)
+    public static Node reverse(Node node, int k)
     {
         //Your code here
-        if(node1==null && node2==null){
-            return true;
+        if(node==null)
+            return null;
+
+        Node head = node;
+
+
+
+        Node pre = null;
+        Node cur = node,post;
+        int count = k;
+        while (cur!=null && count!=0){
+
+            post = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = post;
+
+            --count;
         }
 
-        if(node1!=null && node2!=null && node1.data==node2.data)
-            return isIdentical(node1.left,node2.left) && isIdentical(node1.right,node2.right);
-        else
-            return false;
+        head.next = reverse(cur,k);
+        return pre;
 
     }
-
-
-
-
 }
+
+
+
 
 
 
